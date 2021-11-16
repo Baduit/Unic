c976ee1e4d631ffd22ae33d8169dd7142d54ace1
Baduit <lena.bertho@protonmail.com>
2021-11-16 23:10:46 +0100

    Remove useless comment

--- lib/unic/unic.hpp
+++ lib/unic/unic.hpp
@@ -8,17 +8,6 @@ std::unique_ptr<int> toto;
 namespace unic
 {
 
-/*
-struct StartsWith
-{
-    constexpr auto operator()(std::string_view starter) const
-    {
-        return [starter](const auto& str) { return str.starts_with(starter); };
-    }
-};
-StartsWith starts_with{};
-*/
-
 #define UNIX_GENERATE_PROXY(function) []<typename... Args>(Args&&... args) { return [&]<typename T>(T&& t) { return function(std::forward<T>(t), std::forward<Args>(args)...); } ; }
 
 template <typename T, std::invocable<T> Proxy>
