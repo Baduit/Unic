89482717b55ce3c901ef02fb51f8d6f72fd4e260
Baduit <lena.bertho@protonmail.com>
2021-11-16 23:22:08 +0100

    Indent the macro

--- lib/unic/unic.hpp
+++ lib/unic/unic.hpp
@@ -8,12 +8,16 @@ std::unique_ptr<int> toto;
 namespace unic
 {
 
-#define UNIX_GENERATE_PROXY(function) []<typename... Args>(Args&&... args) { return [&]<typename T>(T&& t) { return function(std::forward<T>(t), std::forward<Args>(args)...); } ; }
+#define UNIX_GENERATE_PROXY(function)                                                                        \
+	[]<typename... Args>(Args && ...args)                                                                    \
+	{                                                                                                        \
+		return [&]<typename T>(T &&t) { return function(std::forward<T>(t), std::forward<Args>(args)...); }; \
+	}
 
 template <typename T, std::invocable<T> Proxy>
-constexpr auto operator->*(T&& t, Proxy&& p)
+constexpr auto operator->*(T &&t, Proxy &&p)
 {
-    return p(std::forward<T>(t));
+	return p(std::forward<T>(t));
 }
 
 } // namespace unic
