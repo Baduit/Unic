e10752fc2191bf4236d1fa34a27deeb03059ec40
Baduit <lena.bertho@protonmail.com>
2021-11-16 23:10:29 +0100

    The macro works \o/

--- lib/unic/unic.hpp
+++ lib/unic/unic.hpp
@@ -1,14 +1,30 @@
 #pragma once
 
 #include <concepts>
+#include <memory>
+
+std::unique_ptr<int> toto;
 
 namespace unic
 {
 
-template <typename T, std::invocable<T> Func>
-constexpr auto operator->*(const T& str, Func&& f)
+/*
+struct StartsWith
 {
-    return f(str);
+    constexpr auto operator()(std::string_view starter) const
+    {
+        return [starter](const auto& str) { return str.starts_with(starter); };
+    }
+};
+StartsWith starts_with{};
+*/
+
+#define UNIX_GENERATE_PROXY(function) []<typename... Args>(Args&&... args) { return [&]<typename T>(T&& t) { return function(std::forward<T>(t), std::forward<Args>(args)...); } ; }
+
+template <typename T, std::invocable<T> Proxy>
+constexpr auto operator->*(T&& t, Proxy&& p)
+{
+    return p(std::forward<T>(t));
 }
 
 } // namespace unic
--- test/unic_tests/unic_test.cpp
+++ test/unic_tests/unic_test.cpp
@@ -5,23 +5,42 @@
 
 #include <unic/unic.hpp>
 
-struct StartsWith
-{
-    constexpr auto operator()(std::string_view starter) const
-    {
-        return [starter](const auto& str) { return str.starts_with(starter); };
-    }
-};
-StartsWith starts_with{};
-
 using unic::operator->*;
 
 
 
-TEST_CASE("Example")
+TEST_CASE("operator->* alone")
 {
+	struct StartsWith
+	{
+		constexpr auto operator()(std::string_view starter) const
+		{
+			return [starter](const auto& str) { return str.starts_with(starter); };
+		}
+	};
+	StartsWith starts_with{};
+
 	constexpr std::string_view str = "lola";
 	constexpr bool b = str->*starts_with("Lolo");
 	static_assert(b == false);
 	static_assert(str->*starts_with("lol"));
+}
+
+constexpr bool my_starts_with(std::string_view str, std::string_view maybe_substr)
+{
+	return str.starts_with(maybe_substr);
+}
+
+TEST_CASE("With a proxy for a function")
+{
+	constexpr auto starts_with = UNIX_GENERATE_PROXY(my_starts_with);
+	constexpr std::string_view str = "lola";
+	{
+		constexpr bool b = str->*starts_with("lol");
+		static_assert(b);
+	}
+	{
+		constexpr bool b = str->*starts_with("loli");
+		static_assert(!b);
+	}
 }
